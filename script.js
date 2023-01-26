37dac6036576e6047893ed247877357da8d53b6d
Padmapriya-Muthu96 <121947075+Padmapriya-Muthu96@users.noreply.github.com>
2023-01-26 22:09:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Write a “person” class to hold all the details.
 class person {
     constructor(name, age, gender, qualification, address) {
         this.name=name;
@@ -12,3 +14,18 @@ return `details are ${name}`;
 }
 var myperson=new person("padma", 26, "female", "BE", "Kanchipuram")
 console.log(myperson);
+
+//write a class to calculate the uber price.
+
+class price {
+    constructor(km, cost) {
+        this.km=km;
+        this.cost=cost;
+        
+    }
+}
+greet = () => {
+return `${km * cost}`;
+}
+var myprice=new price(12, 20)
+console.log(`travel charge is ${12*20}`);
